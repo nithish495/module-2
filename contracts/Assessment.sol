ea4f888d2805c97423dc916507c51bb7fecc4a79
nithish495 <154091132+nithish495@users.noreply.github.com>
2024-01-11 02:05:33 +0530

    Update Assessment.sol

--- contracts/Assessment.sol
+++ contracts/Assessment.sol
@@ -15,12 +15,12 @@ contract Assessment {
         balance = initBalance;
     }
 
-    function getBalance() public view returns(uint256){
+    function getBalance() public view returns (uint256) {
         return balance;
     }
 
     function deposit(uint256 _amount) public payable {
-        uint _previousBalance = balance;
+        uint256 _previousBalance = balance;
 
         // make sure this is the owner
         require(msg.sender == owner, "You are not the owner of this account");
@@ -40,12 +40,13 @@ contract Assessment {
 
     function withdraw(uint256 _withdrawAmount) public {
         require(msg.sender == owner, "You are not the owner of this account");
-        uint _previousBalance = balance;
+        uint256 _previousBalance = balance;
         if (balance < _withdrawAmount) {
-            revert InsufficientBalance({
-                balance: balance,
-                withdrawAmount: _withdrawAmount
-            });
+            revert
+                InsufficientBalance({
+                    balance: balance,
+                    withdrawAmount: _withdrawAmount
+                });
         }
 
         // withdraw the given amount
@@ -57,6 +58,40 @@ contract Assessment {
         // emit the event
         emit Withdraw(_withdrawAmount);
     }
+
+    // Trigonometric function - Sine
+function sin(uint256 _angleInDegrees) public pure returns (int256) {
+    // Convert degrees to radians
+    int256 angleInRadians = (int256(_angleInDegrees) * int256(314159)) / int256(180000);
+
+    // Calculate sine using a basic series expansion
+    int256 result = angleInRadians;
+    int256 term = angleInRadians;
+
+    for (uint256 i = 1; i <= 10; i++) {
+        term = (term * angleInRadians * angleInRadians) / int256((2 * i) * (2 * i + 1));
+        if (i % 2 == 0) {
+            result += term;
+        } else {
+            result -= term;
+        }
+    }
+
+    return result;
 }
 
 
+    // Exponential function
+    function exp(uint256 _power) public pure returns (uint256) {
+        // Calculate e^x using a basic series expansion
+        uint256 result = 1;
+        uint256 term = 1;
+
+        for (uint256 i = 1; i <= 10; i++) {
+            term = (term * _power) / i;
+            result += term;
+        }
+
+        return result;
+    }
+}
